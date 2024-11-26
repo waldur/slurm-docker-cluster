c386447010f038902d3a8e8ccb0f35fe081772ff
Mark Borissov <mark.borissov@ut.ee>
2024-11-26 10:29:01 +0200

    Add allocation cleanup script for cron

---
+++ delete_hpc_accounts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Delete all existing slurm accounts.
+
+accounts=$(sacctmgr show account format=Account --parsable2 -n | grep '^hpc_')
+
+for account in $accounts; do
+	echo "Deleting acc $account"
+	sacctmgr delete account name=$account --immediate
+done
+
+echo "All hpc accounts deleted from slurm cluster."
