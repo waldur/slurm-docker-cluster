501c51740679eaca124aa8846118b60434179777
Mark Borissov <mark.borissov@ut.ee>
2024-12-03 15:15:58 +0200

    Add delete script to PATH

--- delete_hpc_accounts.sh
+++ delete_hpc_accounts.sh
@@ -7,6 +7,7 @@ accounts=$(sacctmgr show account format=Account --parsable2 -n | grep '^hpc_')
 for account in $accounts; do
     echo "Canceling jobs for account $account"
     scancel --account="$account"
+	
 	echo "Deleting acc $account"
 	sacctmgr delete account name="$account" --immediate
 done
